ad200c868f8990c012f2a67909aa611e03d98f92
Francisco Astorga <fastorga@nimok.com>
2019-09-20 21:56:47 -0300

    Project Boost First Commit
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

---
+++ Assets/Rocket.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 15f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
+
+    bool collisionsEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {                   
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+
+        if (Debug.isDebugBuild)
+            RespondToDebugKeys();
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            LoadNextLevel();
+        else if (Input.GetKeyDown(KeyCode.C))        
+            collisionsEnabled = !collisionsEnabled;
+        
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        if (!audioSource.isPlaying)
+            audioSource.PlayOneShot(mainEngine);
+
+        mainEngineParticles.Play();
+    }
+    
+    private void RespondToRotateInput()
+    {   
+        if (Input.GetKey(KeyCode.A))        
+            RotateManually(rcsThrust * Time.deltaTime);        
+        else if (Input.GetKey(KeyCode.D))        
+            RotateManually(-rcsThrust * Time.deltaTime);    
+    }
+
+    private void RotateManually(float rotationThisFrame)
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame);
+        rigidBody.freezeRotation = false; //resume physics rotation
+    }
+
+    //In the Unity Execution Order, OnCollisionEnter is executed every frame BEFORE Update()
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || !collisionsEnabled)
+            return;
+
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":                
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay); //parameterise time
+    }
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        mainEngineParticles.Stop();
+        deathParticles.Play();
+        Invoke("ReloadThisLevel", levelLoadDelay);
+    }    
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if ((nextSceneIndex + 1) > SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadThisLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
